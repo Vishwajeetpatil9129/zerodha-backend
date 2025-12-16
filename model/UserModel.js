9a0819ed7e1c7c11ea08d33b726c8fe29a0ee4e4
Vishwajeet Patil <vishwjitpatil01@gmail.com>
2025-12-16 23:43:13 +0530

    Initial backend commit

---
+++ model/UserModel.js
@@ -0,0 +1,25 @@
+const { model, Schema } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const UserModel = model("user", UserSchema);
+
+module.exports = { UserModel };
